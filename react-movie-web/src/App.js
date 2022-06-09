66a582ffd17f2674c0b319a41a1130c83b51149b
bnbnac <bnbnac@naver.com>
2022-06-09 10:36:08 +0900

    effect and dependency

--- react-movie-web/src/App.js
+++ react-movie-web/src/App.js
@@ -1,11 +1,30 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((current) => current + 1);
+  const onChange = (event) => setKeyword((current) => event.target.value);
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("i run only once");
+  }, []);
+  useEffect(() => {
+    console.log("i run when keyword changes");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("i run when counter changes");
+  }, [counter]);
   return (
     <div>
-      <h1 className={styles.title}>welcome react</h1>
-      <Button text={"Continue"} />
+      <input
+        valuu={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="search here"
+      ></input>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Hi</button>
     </div>
   );
 }
